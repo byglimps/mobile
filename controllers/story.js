1f45d9cc39fa5748d38ae3393b3701a34a579d44
Juan Alvarado <jalvarado91@gmail.com>
2018-08-05 22:07:15 -0400

    fixes endpoints to reflect new api structure

--- controllers/story.js
+++ controllers/story.js
@@ -1,16 +1,14 @@
 import axios from "react-native-axios";
 
-const BASE_URL = "http://6a6f54ec.ngrok.io/api/glimps";
+const BASE_URL = "http://6a6f54ec.ngrok.io";
 
 const story = axios.create({ baseURL: BASE_URL });
 
 const createCollage = async pictures => {
   const eventId = "2a50ee1a-6add-419e-ae8a-da6621e035d5";
+  const url = `/api/events/${eventId}/glimpses`;
   try {
-    const { data } = await story.post("/", {
-      eventId: eventId,
-      data: pictures
-    });
+    const { data } = await story.post(url, { data: pictures });
 
     if (data.success) {
       return { collage: data.data.thumbUrl };
